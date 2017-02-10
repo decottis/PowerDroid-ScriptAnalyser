f845f4ad9d2a8c2a0e30f4376199dad66510a875
Simon <simon@Simon.(none)>
2017-02-10 15:21:33 +0100

    [init] add script wich collect monsoon data

---
+++ collectData.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import csv
+import sys
+import monsoon
+import os
+import time
+
+"""
+script to get data from the monitor
+
+argv[1] csv file
+argv[2] monitor file
+argv[3] frequency
+argv[4] -s, only setup
+"""
+class MyMonsoon:
+
+	voltage = 3.7
+
+	"""
+	argv[2] --> file monitor (ex:/dev/ttyACM0)
+	"""
+	def __init__(self):
+		os.chmod(sys.argv[2], 0666)
+
+	"""
+	write data from the monitor in a csv file (sys.argv[1])
+	"""
+	def writeCSV(self):
+		hz = sys.argv[3]
+		mydata = []
+		cpt = 0
+		c = csv.writer(open(sys.argv[1], "wb"))
+
+		""" add collumn title """
+		c.writerow(["tick", "W"])
+
+		self.mon.StartDataCollection()
+		while True:
+			data = self.mon.CollectData()
+			cptArray = 0
+
+			print((sum(data) / len(data)) * self.voltage)
+			c.writerow([cpt, (sum(data) / len(data)) * self.voltage])
+
+			#while cptArray < len(data):
+			#	print(str(cpt) + " --> " + str(data[cptArray]))
+			#	c.writerow([cpt, data[cptArray]])
+			#	cpt = cpt + 1
+			#	cptArray = cptArray + 1
+			#	time.sleep(float(hz))
+
+			time.sleep(float(hz))
+			cpt = cpt + 1
+
+		self.mon.StopDataCollection()
+
+
+
+	"""
+	setup monitor
+	"""
+	def setupMonitor(self):
+		self.mon = monsoon.Monsoon()
+		self.mon.SetVoltage(self.voltage)
+		print(self.mon.GetStatus())
+
+
+
+if __name__ == '__main__':
+	m = MyMonsoon()
+
+	if(len(sys.argv) == 5):
+		if(sys.argv[4] == "-s"):
+			m.setupMonitor()
+	else:
+		m.setupMonitor()
+		m.writeCSV()
